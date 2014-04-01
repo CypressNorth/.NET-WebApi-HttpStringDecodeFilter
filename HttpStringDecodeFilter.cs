5ad6a5a93fc7aa93bae483d6b6802ae25d91ceb2
jonnale <jonnalewajek@gmail.com>
2014-04-01 16:20:23 -0400

    Update HttpStringDecodeFilter.cs
    Removed unused using statements.

--- HttpStringDecodeFilter.cs
+++ HttpStringDecodeFilter.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Reflection;
-using System.Web;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
 
